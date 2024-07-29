674de9abb9a1898fe714b4fb6150a8da0840fd1c
sanjayaju <sanjayaju789@gmail.com>
2024-07-29 00:18:37 -0700

    New update

--- controllers/bannerCtrl.js
+++ controllers/bannerCtrl.js
@@ -1,7 +1,6 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 const Banners = require('../models/bannerModal');
-
 
 const loadBannerList = async (req, res, next) => {
     try {
@@ -16,102 +15,99 @@ const loadBannerList = async (req, res, next) => {
     }
 };
 
-
-
- 
-const addBanner = async(req, res, next) => {
+const addBanner = async (req, res, next) => {
     try {
         const { heading, url } = req.body;
-        const image = req.file.filename
+        const image = req.file.filename;
 
         await new Banners({
             heading, url, image
         }).save();
 
-        res.redirect('/admin/banners')
-        
-    } catch (error) {
-        next(error)
-    }
-}
-const UpdateBanner = async(req, res, next) => {
-    try {
-
-        const bannerId = req.params.bannerId
-        const { heading, url } = req.body;
-
-        let image = false;
-        if(req.file){
-            image = req.file.filename
-        }
-
-        if(image){
-
-            const bannerData = await Banners.findById({_id: bannerId})
-
-            fs.unlink(path.join(__dirname,'../public/images/bannerImages/', bannerData.image), (err) => {
-                if(err) next(err)
-            });
-
-            await Banners.findByIdAndUpdate(
-                {_id: bannerId},
-                {
-                    $set:{
-                        heading, image , url
-                    }
-                }    
-            );
-
-        }else{
-            
-            await Banners.findByIdAndUpdate(
-                {_id: bannerId},
-                {
-                    $set:{
-                        heading, url
-                    }
-                }    
-            );
-        }
-
         res.redirect('/admin/banners');
-
     } catch (error) {
-        next(error)
+        next(error);
     }
-}
+};
 
-const deleteBanner = async(req, res, next) => {
+const UpdateBanner = async (req, res, next) => {
     try {
-
         const bannerId = req.params.bannerId;
+        const { heading, url } = req.body;
+        let image = false;
 
-        const bannerData = await Banners.findById({_id: bannerId})
-        
-        //Deleting image from bannerImages
-        fs.unlink(path.join(__dirname,'../public/images/bannerImages/', bannerData.image), (err) => {
-            if(err) next(err)
-        });
+        if (req.file) {
+            image = req.file.filename;
+        }
 
-        //Deleting banner document
-        await Banners.findByIdAndDelete({_id:bannerId})
+        const bannerData = await Banners.findById({ _id: bannerId });
 
-        res.redirect('/admin/banners')
+        if (image) {
+            const oldImagePath = path.join(__dirname, '../public/images/bannerImages/', bannerData.image);
 
+            if (fs.existsSync(oldImagePath)) {
+                fs.unlink(oldImagePath, async (err) => {
+                    if (err) {
+                        return next(err);
+                    }
+
+                    await Banners.findByIdAndUpdate(
+                        { _id: bannerId },
+                        { $set: { heading, image, url } }
+                    );
+
+                    res.redirect('/admin/banners');
+                });
+            } else {
+                console.log('File does not exist:', oldImagePath);
+                await Banners.findByIdAndUpdate(
+                    { _id: bannerId },
+                    { $set: { heading, image, url } }
+                );
+
+                res.redirect('/admin/banners');
+            }
+        } else {
+            await Banners.findByIdAndUpdate(
+                { _id: bannerId },
+                { $set: { heading, url } }
+            );
+
+            res.redirect('/admin/banners');
+        }
     } catch (error) {
-        next(error)
+        next(error);
     }
-}
+};
 
+const deleteBanner = async (req, res, next) => {
+    try {
+        const bannerId = req.params.bannerId;
+        const bannerData = await Banners.findById({ _id: bannerId });
+        const imagePath = path.join(__dirname, '../public/images/bannerImages/', bannerData.image);
 
+        if (fs.existsSync(imagePath)) {
+            fs.unlink(imagePath, async (err) => {
+                if (err) {
+                    return next(err);
+                }
 
+                await Banners.findByIdAndDelete({ _id: bannerId });
+                res.redirect('/admin/banners');
+            });
+        } else {
+            console.log('File does not exist:', imagePath);
+            await Banners.findByIdAndDelete({ _id: bannerId });
+            res.redirect('/admin/banners');
+        }
+    } catch (error) {
+        next(error);
+    }
+};
 
-
-
-
-module.exports={
+module.exports = {
     loadBannerList,
     addBanner,
     UpdateBanner,
     deleteBanner
-}
+};
